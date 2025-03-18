e9914b2d74be9dbfd0ff7b41e81e2c86fc01bb4a
Josh Harris <me@celant.co.uk>
2025-03-18 16:07:09 +0000

    Update scripts

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,8 +1,9 @@
 @echo off
 
+echo Setting up post-merge hooks
 type NUL > .git/hooks/post-merge
 echo #!/bin/sh > .git/hooks/post-merge
-echo java -jar InstanceSync.jar >> .git/hooks/post-merge
+echo java -jar ./automation/InstanceSync.jar >> .git/hooks/post-merge
 
 echo Done setting up hooks
 echo Running InstanceSync
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,9 @@
+#!/bin/sh
+
+echo "Setting up post-merge hooks"
 echo "#!/bin/sh" > .git/hooks/post-merge
-echo "java -jar InstanceSync.jar" >> .git/hooks/post-merge
+echo "java -jar ./automation/InstanceSync.jar" >> .git/hooks/post-merge
+chmod +x .git/hooks/post-merge
 
 echo "Done setting up hooks"
 echo "Running InstanceSync"
